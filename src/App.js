2c40757e1d7d13ee9d77774ab795586ca8afd29c
Bendada-abdelmajid <abdelmajid.bendada1998@gmail.com>
2023-04-21 15:15:08 +0000

    fix cros

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, { useState , useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Home from "./pages/Home";
 import ChateRome from "./pages/ChateRome";
 import api from "./api/local";
@@ -7,53 +7,37 @@ import { userContext } from "./context/userContext";
 import LodingBage from "./components/LodingBage";
 
 function App() {
-  const [isLoding , setIsLoding]= useState(true)
- 
+  const [isLoding, setIsLoding] = useState(true);
+  
+  api.defaults.withCredentials = true;
   useEffect(() => {
-    const getUser = () => {
-      
-      
-      
-      
-      fetch(process.env.REACT_APP_API_URL+"/auth/check-user", {
-        method: "GET",
-        credentials: "include",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Credentials": true,
-        },
-      })
-        .then((response) => {
-          if (response.status === 200) return response.json();
-          throw new Error("authentication has been failed!");
-        })
-        .then((resObject) => {
-          setUser(resObject.user._json);
-          setTimeout(()=>{
-                  setIsLoding(false)
-          }, 2000)
-        })
-        .catch((err) => {
-          
-        });
+    const getUser = async () => {
+      const response = api.get(
+        process.env.REACT_APP_API_URL + "/auth/check-user"
+      );
+      console.log(response)
+      if (response.status === 200) {
+        const res = await response.json();
+        setUser(res.user._json);
+       
+      }
+
+      setTimeout(() => {
+        setIsLoding(false);
+      }, 2000);
     };
     getUser();
   }, []);
-  
-  const [user , setUser]= useState(null)
- 
-  api.defaults.withCredentials=true
+
+  const [user, setUser] = useState(null);
+
+  api.defaults.withCredentials = true;
   return (
     <div className="App">
-     
-        
-      
-      <userContext.Provider value={{ user , setUser }}>
-      <LodingBage isLoding={isLoding}/>
-        {user === null?<Home/>:<ChateRome/>}
+      <userContext.Provider value={{ user, setUser }}>
+        <LodingBage isLoding={isLoding} />
+        {user === null ? <Home /> : <ChateRome />}
       </userContext.Provider>
-    
     </div>
   );
 }
